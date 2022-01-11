b75348d9fd9b7b0dc311f48262239bd9f70b92cb
Nolan Caine <nolancaine@gmail.com>
2022-01-11 00:07:23 -0800

    Import scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+
 import ContextProvider from './context/ContextProvider';
 import App from './components/App';
+import './styles/index.scss';
 
 const root: HTMLElement | null = document.getElementById('root');
 
