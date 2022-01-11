c42fff98cb74c615c43e43b7c694c9fb8910dd6f
Nolan Caine <nolancaine@gmail.com>
2022-01-11 13:46:33 -0800

    Trigger Detail modal on click

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { IArticle } from '../models/interfaces';
 import dayjs from 'dayjs';
+
+import { Context } from '../context/ContextProvider';
+import { IArticle } from '../models/interfaces';
 
 const ArticleCard = ({
   section, subsection, title, abstract, url, uri, byline, item_type,
   updated_date, created_date, published_date, material_type_facet, kicker,
   des_facet, org_facet, per_facet, geo_facet, multimedia, short_url,
 }: IArticle) => {
+
+  const { showDetailView } = useContext(Context);
 
   const formattedSection = subsection.length
     ? `${section} / ${subsection}`
@@ -22,6 +26,11 @@ const ArticleCard = ({
         style={{
           backgroundImage: `url(${multimedia[0].url})`
         }}
+        onClick={ (event) => showDetailView(event, {
+          section, subsection, title, abstract, url, uri, byline, item_type,
+          updated_date, created_date, published_date, material_type_facet, kicker,
+          des_facet, org_facet, per_facet, geo_facet, multimedia, short_url,
+        }) }
       >
         <div className='article-card__text-overlay'>
           <div className='article-card__text-overlay__tags'>
