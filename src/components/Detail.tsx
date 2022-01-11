3d10d131ace4e8c01311e27de8e5b6c8bdf16486
Nolan Caine <nolancaine@gmail.com>
2022-01-11 13:46:04 -0800

    Create Detail component with modal

---
+++ src/components/Detail.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useContext } from 'react';
+import ReactModal from 'react-modal';
+import dayjs from 'dayjs';
+
+import { Context } from '../context/ContextProvider';
+
+ReactModal.setAppElement('#root');
+
+const inlineStyles: ReactModal.Styles = {
+  overlay: {
+
+  },
+  content: {
+    textAlign: 'center'
+  }
+}
+
+const Detail = () => {
+  const { selectedArticle, modalIsOpen, hideDetailView } = useContext(Context);
+
+  const formattedPublishDate = dayjs(selectedArticle?.published_date).format('MM/DD/YYYY');
+  const formattedUpdateDate = dayjs(selectedArticle?.updated_date).format('MM/DD/YYYY');
+
+  return (
+    <ReactModal
+      isOpen={ modalIsOpen }
+      onRequestClose={ hideDetailView }
+      shouldCloseOnOverlayClick={ true }
+      style={ inlineStyles }
+    >
+      { selectedArticle &&
+        <>
+        <h2 className='detail-container__modal__title' >{ selectedArticle.title }</h2>
+        <h4 className='detail-container__modal__byline'>{ selectedArticle.byline }</h4>
+        <h5 className='detail-container__modal__dates'>Published { formattedPublishDate } • Updated { formattedUpdateDate }</h5>
+        <p className='detail-container__modal__abstract'>{ selectedArticle.abstract }</p>
+        <img className='detail-container__modal__image' src={ selectedArticle.multimedia[0].url } />
+        </>
+      }
+    </ReactModal>
+  )
+}
+
+export default Detail;
