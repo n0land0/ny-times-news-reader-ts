ab25b9a63cca93399bc682075a64a225ba2f9e41
Nolan Caine <nolancaine@gmail.com>
2022-01-12 00:28:48 -0800

    Add helper functions and useEffect for view change

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,33 +1,38 @@
 import React, { useState, useEffect, useContext, createContext } from 'react';
+
 import { IContextValue, IArticle } from '../models/interfaces';
-
-import { fetchHomeArticles } from '../apiCalls';
-
-export const Context = createContext<IContextValue>({
-  view: 'home',
-  displayList: [],
-  selectedArticle: null,
-  modalIsOpen: false,
-  showDetailView: () => {},
-  hideDetailView: () => {}
-})
-
-// api returns an array of article objects (no id) from the specified section
-  // when article is clicked, find it
-
-// what do I need in app state?
-  // filter states for landing page source - home (default), arts, politics, etc
-  // currently viewed article -
+import { Context } from './Context';
+import { fetchHomeArticles, fetchArticles } from '../apiCalls';
 
 const ContextProvider = ({ children }: React.PropsWithChildren<{}>) => {
   const [ view, setView ] = useState<string>('home');
   const [ displayList, setDisplayList ] = useState<IArticle[]>([]);
   const [ selectedArticle, setSelectedArticle ] = useState<IArticle | null>(null);
   const [ modalIsOpen, setModalIsOpen ] = useState(false);
+  const [ filterTags, setFilterTags ] = useState<string[]>([]);
 
   const getHomeArticles = async () => {
     const homeArticles = await fetchHomeArticles();
     setDisplayList(homeArticles);
+  }
+
+  // const getArticles = async (filterTags: string[]) => {
+  //   const articlesToDisplay: IArticle[] = [];
+  //   filterTags.forEach((sectionName: string) => {
+  //     fetchArticles(sectionName)
+  //       .then((resultsArray: IArticle[]) => {
+  //         articlesToDisplay.push(...resultsArray)
+  //         // setDisplayList([...displayList, ...resultsArray]);
+  //       })
+  //       .then(() => {
+  //         setDisplayList(articlesToDisplay);
+  //       })
+  //   })
+  // }
+
+  const getArticles = async (view: string) => {
+    const sectionArticles: IArticle[] = await fetchArticles(view);
+    setDisplayList(sectionArticles);
   }
 
   const showDetailView = (event: React.MouseEvent, props: IArticle) => {
@@ -40,14 +45,36 @@ const ContextProvider = ({ children }: React.PropsWithChildren<{}>) => {
     setSelectedArticle(null);
   }
 
+  const addFilterTag = (tagToAdd: string) => {
+    setFilterTags([...filterTags, tagToAdd]);
+  }
+
+  const removeFilterTag = (tagToRemove: string) => {
+    const updatedFilterTags = filterTags.filter((tag: string) =>
+      tag !== tagToRemove
+    );
+    setFilterTags(updatedFilterTags);
+  }
+
   useEffect(() => {
     getHomeArticles()
   }, [])
 
+  // useEffect(() => {
+  //   filterTags.length
+  //     ? getArticles(filterTags)
+  //     : getHomeArticles()
+  // }, [ filterTags ])
+
+  useEffect(() => {
+    getArticles(view)
+  }, [ view ])
+
   return (
     <Context.Provider value={{
-      view, displayList, selectedArticle, modalIsOpen,
-      showDetailView, hideDetailView
+      view, setView, displayList, selectedArticle, modalIsOpen,
+      showDetailView, hideDetailView, filterTags, setFilterTags,
+      addFilterTag, removeFilterTag
     }}>
       { children }
     </Context.Provider>
