621d20ff01555e0d18bfdfaf49da8ce0d856e72f
Nolan Caine <nolancaine@gmail.com>
2022-01-12 00:27:06 -0800

    Add all section options from API reference

---
+++ src/data/sections.ts
@@ -0,0 +1,3 @@
+export const sections: string[] = [
+  'home', 'arts', 'automobiles', 'books', 'business', 'fashion', 'food', 'health', 'insider', 'magazine', 'movies', 'nyregion', 'obituaries', 'opinion', 'politics', 'realestate', 'science', 'sports', 'sundayreview', 'technology', 'theater', 't-magazine', 'travel', 'upshot', 'us', 'world'
+]
